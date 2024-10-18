8d9f4ca34e1908737df0e6a1fb93d54e247838f4
LautaroVera03 <lautaroveg12@gmail.com>
2024-10-18 18:41:57 -0300

    Se modifico el home y la url de la aplicacion de registro de productos

--- DeliciasManá/DeliciasManá/urls.py
+++ DeliciasManá/DeliciasManá/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gestion_productos/', include('apps.gestion_productos.urls')),
+    path('gestion_productos', include('apps.gestion_productos.urls')),
     path('gestion_insumos', include('apps.gestion_insumos.urls')),
+    path('', TemplateView.as_view(template_name='base/PaginaPortada.html'), name='home')
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- DeliciasManá/apps/gestion_productos/urls.py
+++ DeliciasManá/apps/gestion_productos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'apps.gestion_productos'
 urlpatterns = [
-    path('productos/', views.registrar_productos)
+    path('', views.registrar_productos)
 ]
 
