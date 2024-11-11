832e74faf223de8b6a981a7488cfb40ff21415b4
LautaroVera03 <lautaroveg12@gmail.com>
2024-11-11 12:14:04 -0300

    se solucionó clientes mayoristas y empleados

--- DeliciasManá/apps/gestion_empleados/views.py
+++ DeliciasManá/apps/gestion_empleados/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EmpleadoForm
 from django.http import HttpResponse
 from django.urls import reverse
@@ -17,9 +17,37 @@ def formulario(request):
             return redirect(reverse('apps.gestion_empleados:formulario'))
         else:
             messages.error(request, "Error al guardar el formulario. Verifica los campos.")
+            # Mostrar los errores del formulario
+            for field, errors in empleado_form.errors.items():
+                for error in errors:
+                    print(f"Error en el campo {field}: {error}")
     else:
         empleado_form = EmpleadoForm()
+
     empleados = Empleado.objects.all()
     return render(request, 'gestion_empleados/gestion_empleados.html', {'form': empleado_form, 'empleados': empleados})
 
-    return HttpResponse("Error: Solicitud no procesada correctamente.")
+
+def modificar_empleado(request, empleado_id):
+    empleado = get_object_or_404(Empleado, id=empleado_id)  # Obtener el empleado por ID
+    if request.method == 'POST':
+        empleado_form = EmpleadoForm(request.POST, instance=empleado)  # Pasar la instancia al formulario
+        if empleado_form.is_valid():
+            empleado_form.save()  # Guardar el empleado modificado
+            return redirect('apps.gestion_empleados:formulario')  # Redirigir después de guardar
+    else:
+        empleado_form = EmpleadoForm(instance=empleado)  # Crear el formulario con la instancia existente
+    empleados = Empleado.objects.all().order_by('id')
+    return render(request, 'gestion_empleados/gestion_empleados.html', {'form': empleado_form, 'empleados': empleados})
+
+
+def eliminar_empleado(request, empleado_id):
+    empleado = get_object_or_404(Empleado, id=empleado_id)  # Obtener el empleado por ID
+
+    if request.method == 'POST':
+        empleado.delete()  # Eliminar el empleado
+        messages.success(request, 'Empleado eliminado exitosamente.')
+        return redirect('apps.gestion_empleados:formulario')  # Redirigir después de eliminar
+
+    empleados = Empleado.objects.all().order_by('id')
+    return render(request, 'gestion_empleados/gestion_empleados.html', {'empleados': empleados})
